77f622ddd3ab16e85e852443e3fca2c30f176451
Jan Nijboer <jrnijboer@gmail.com>
2015-08-21 23:29:06 +0200

    initial commit

---
+++ SteamStopperService/ProjectInstaller.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace SteamStopperService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.SteamServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            this.serviceProcessInstaller.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceProcessInstaller1_AfterInstall);
+            // 
+            // SteamServiceInstaller
+            // 
+            this.SteamServiceInstaller.DelayedAutoStart = true;
+            this.SteamServiceInstaller.Description = "This service runs a webservice that can stop the Steam client running in another " +
+    "users session";
+            this.SteamServiceInstaller.DisplayName = "SteamStopperService";
+            this.SteamServiceInstaller.ServiceName = "SteamStopperService";
+            this.SteamServiceInstaller.AfterInstall += new System.Configuration.Install.InstallEventHandler(this.serviceInstaller1_AfterInstall);
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.SteamServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller SteamServiceInstaller;
+    }
+}
---
+++ SteamStopperService/SteamService.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.ServiceProcess;
+using System.Text.RegularExpressions;
+
+namespace SteamStopperService
+{
+    public partial class SteamService : ServiceBase
+    {
+        private HttpListener listener;       
+        
+        public SteamService()
+        {
+            InitializeComponent();
+            this.ServiceName = "SteamService";            
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                listener = new HttpListener();
+                listener.Prefixes.Add("http://localhost:8088/steam/");
+                listener.Start();
+                StartListening(listener);
+            }
+            catch(Exception)
+            {                
+                listener.Stop();
+            }
+            finally
+            {
+                
+            }
+        }
+
+        protected override void OnStop()
+        {            
+            try
+            {
+                listener.Stop();
+            }
+            finally
+            {
+                listener.Close();
+            }
+        }
+
+        private async void StartListening(HttpListener listener)
+        {
+            while (listener.IsListening)
+            {
+                try
+                {                    
+                    var context = await listener.GetContextAsync();                 
+                    var request = context.Request;
+                    string username = Regex.Match(request.RawUrl, "steam/(.*)/stop$").Groups[1].Value;
+                    string responseString = "";
+                    if (request.RawUrl.EndsWith("/stop"))
+                    {
+                        var proc = GetProcessInfoByWMI("steam");
+                        if (proc == null)
+                        {
+                            responseString = "steam not running";
+                        }
+                        else
+                        {
+                            if (username.ToLower() != proc.Username.ToLower())
+                            {
+                                StopProcess(proc.ProcessId);
+                                responseString = "steam stopped, pid: " + proc.ProcessId.ToString();
+                            }
+                            else
+                            {
+                                responseString = "already running steam process on user: " + proc.Username;
+                            }
+                        }
+                    }
+
+                    var response = context.Response;
+
+                    byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+                    response.ContentLength64 = buffer.Length;
+                    using (System.IO.Stream output = response.OutputStream)
+                    {
+                        output.Write(buffer, 0, buffer.Length);
+                    }                    
+                }
+
+                catch (Exception)
+                {                    
+                }
+            }
+        }
+
+        private static ProcessInfo GetProcessInfoByWMI(string processName)
+        {
+            ManagementObjectSearcher Processes = new ManagementObjectSearcher("SELECT * FROM Win32_Process");
+
+            var processes = Process.GetProcesses();
+            var WMIProcessesList = Processes.Get().Cast<ManagementObject>().ToList();
+
+            var searchedProcess = processes.FirstOrDefault(p => p.ProcessName.ToLower() == processName);
+
+            if (searchedProcess != null)
+            {
+                var wmiproc = WMIProcessesList.FirstOrDefault(proc => (int)(uint)proc["ProcessId"] == searchedProcess.Id);
+
+                var OwnerInfo = new string[2];
+                wmiproc.InvokeMethod("GetOwner", OwnerInfo);
+                string user = OwnerInfo[0];
+
+                return new ProcessInfo() { ProcessId = searchedProcess.Id, ProcessName = searchedProcess.ProcessName, Username = user };
+            }
+            return null;
+        }
+
+        private static void StopProcess(int processid)
+        {
+            var process = Process.GetProcessById(processid);
+            if (process != null)
+            {
+                process.Kill();
+            }
+        }
+    }
+
+    class ProcessInfo
+    {
+        public string Username { get; set; }
+        public int ProcessId { get; set; }
+        public string ProcessName { get; set; }
+    }
+}
